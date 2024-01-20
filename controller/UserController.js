90d3e8e3607fa8dbb35a33c039ebd9a3373fc1d5
JOSHUA-MUANLAL <105428544+JOSHUA-MUANLAL@users.noreply.github.com>
2024-01-20 20:02:42 +0530

    Update UserController.js

--- controller/UserController.js
+++ controller/UserController.js
@@ -48,95 +48,80 @@ router.get('/register',async (req,res)=>{
     res.render('Register');
 })  
 
-router.post('/register',async (req,res)=>{
-    const uid=req.body.userId;
-    const name=req.body.userName;
-    const bankaccount=req.body.BankAccount;
-   
+router.post('/register', async (req, res) => {
+    const uid = req.body.userId;
+    const name = req.body.userName;
+    const bankaccount = req.body.BankAccount;
+
     const user = await UserModel.findOne({ user_id: uid });
 
-    if(user){
-        let msg="User Id already registered";
-        res.render('Register',{msg})
-    }else{
-        await insertData(req,res);
+    if (user) {
+        let msg = "User Id already registered";
+        res.render('Register', { msg });
+    } else {
+        try {
+            await insertData(req, res);
+        } catch (error) {
+            console.error("Error in insertData:", error);
+            res.status(500).send('Internal Server Error');
+        }
     }
+});
 
-    
-  })
+async function insertData(req, res) {
+    try {
+        let bankid = req.body.BankAccount;
+        if (ifsc.validate(bankid) == false) {
+            let msg = "Invalid IFSC";
+            res.render('Register', { msg });
+            return;
+        }
 
+        const result = await ifsc.fetchDetails(bankid);
 
-  async function insertData(req,res){
-
-   try{ let bankid=req.body.BankAccount;
-    if(ifsc.validate(bankid)==false){
-        let msg="Invalid IFSC";
-        res.render('Register',{msg});
-    }
-    await ifsc.fetchDetails(bankid).then(function(result) {
-        request(`https://api.openweathermap.org/data/2.5/weather?q=${result.CITY}&appid=4de4db30a24565b1ba581198fbedf9e4`,(err,response,body)=>{
+        request(`https://api.openweathermap.org/data/2.5/weather?q=${result.CITY}&appid=4de4db30a24565b1ba581198fbedf9e4`, async (err, response, body) => {
             if (err) {
                 console.error("Error in Weather API request:", err);
-                // Handle the error response to the client if needed
-                res.status(500).send('could fetch temperature 81');
+                res.status(500).send('Internal Server Error');
                 return;
             }
 
-
-            if(response.statusCode===200){
-
-                let data=JSON.parse(body);
-                const user= new UserModel({
-                        user_id:req.body.userId,
-                        user_name: req.body.userName,
-                        bank_accounts: req.body.BankAccount,
-                        name: req.body.userName,
-                        accounts: {
-                          bank:result.BANK,
-                          branch: result.BRANCH,
-                          address: result.ADDRESS,
-                          city: result.CITY,
-                          district: result.DISTRICT,
-                          state: result.STATE,
-                          bank_code: result.BANKCODE,
-                          weather: {
+            if (response.statusCode === 200) {
+                let data = JSON.parse(body);
+                const user = new UserModel({
+                    user_id: req.body.userId,
+                    user_name: req.body.userName,
+                    bank_accounts: req.body.BankAccount,
+                    name: req.body.userName,
+                    accounts: {
+                        bank: result.BANK,
+                        branch: result.BRANCH,
+                        address: result.ADDRESS,
+                        city: result.CITY,
+                        district: result.DISTRICT,
+                        state: result.STATE,
+                        bank_code: result.BANKCODE,
+                        weather: {
                             temp: data.main.temp,
                             humidity: data.main.humidity
-                          }
                         }
-                })
-                
-            
-                user.save()
-                .then(result => {
-                    // Handle the result
-                    console.log("done registration",result);
-                    let data="registration done";
-                    res.render('home',{})
-                  })
-                  .catch(err => {
-                    // Handle the error
-                    console.error("Faild to register db",err);
-                  });
+                    }
+                });
 
-
-                
-
-            }else {
-                // Handle non-200 status code
+                await user.save();
+                console.log("done registration", user);
+                res.render('home', {});
+            } else {
                 console.error("Weather API returned non-200 status code:", response.statusCode);
-                // Send an error response to the client if needed
-                res.status(500).send('Internal Server Error 129');
+                res.status(500).send('Internal Server Error');
             }
-        }) 
+        });
+    } catch (error) {
+        console.log("error in fetching bank data", error);
+        res.status(500).send('Internal Server Error');
+    }
+}
 
-
-     });
-   }catch(error){
-    console.log("error in fetching bank data",error)
-   }
-
- }
 
 
 

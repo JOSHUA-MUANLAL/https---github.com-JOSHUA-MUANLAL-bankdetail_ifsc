62cc570e868a412805341f2e3de413a63ecdd28d
JOSHUA MUANLAL <tonsingjoshuamuanlal>
2024-01-20 11:40:40 +0530

    first commit

---
+++ controller/UserController.js
@@ -0,0 +1,139 @@
+const router = require("express").Router();
+const UserModel = require('../dbmodel/db');
+const mongoose=require('mongoose');
+const ejs = require("ejs");
+const ifsc=require('ifsc');
+const request=require('request');
+
+
+router.get('/',(req,res)=>{
+    res.render('views/home');
+
+})
+
+router.get('/getDetail',(req,res)=>{
+    res.render('views/ViewData')
+})
+
+router.post('/getDetail',(req,res)=>{
+    const id=req.body.userId;
+    
+
+    res.redirect(`/getDetail/${id}`);
+})
+
+router.get('/getdetail/:id',async (req,res)=>{
+    const id=req.params.id;
+
+    const data = await UserModel.findOne({ user_id: id });
+
+    if(data){
+        res.render('views/data',{data});
+    }else{
+        let msg="No user Found, Please register First"
+        res.render('views/Register');
+    }
+
+  
+
+    
+  })
+
+router.get('/register',async (req,res)=>{
+    res.render('views/Register');
+})  
+
+router.post('/register',async (req,res)=>{
+    const uid=req.body.userId;
+    const name=req.body.userName;
+    const bankaccount=req.body.BankAccount;
+   
+    const user = await UserModel.findOne({ user_id: uid });
+
+    if(user){
+        let msg="User Id already registered";
+        res.render('views/Register',{msg})
+    }else{
+        await insertData(req,res);
+    }
+
+    
+  })
+
+
+  async function insertData(req,res){
+
+   try{ let bankid=req.body.BankAccount;
+    if(ifsc.validate(bankid)==false){
+        let msg="Invalid IFSC";
+        res.render('views/register',{msg});
+    }
+    await ifsc.fetchDetails(bankid).then(function(result) {
+        request(`https://api.openweathermap.org/data/2.5/weather?q=${result.CITY}&appid=4de4db30a24565b1ba581198fbedf9e4`,(err,response,body)=>{
+            if (err) {
+                console.error("Error in Weather API request:", err);
+                // Handle the error response to the client if needed
+                res.status(500).send('Internal Server Error');
+                return;
+            }
+
+
+            if(response.statusCode===200){
+
+                let data=JSON.parse(body);
+                const user= new UserModel({
+                        user_id:req.body.userId,
+                        user_name: req.body.userName,
+                        bank_accounts: req.body.BankAccount,
+                        name: req.body.userName,
+                        accounts: {
+                          bank:result.BANK,
+                          branch: result.BRANCH,
+                          address: result.ADDRESS,
+                          city: result.CITY,
+                          district: result.DISTRICT,
+                          state: result.STATE,
+                          bank_code: result.BANKCODE,
+                          weather: {
+                            temp: data.main.temp,
+                            humidity: data.main.humidity
+                          }
+                        }
+                })
+                
+            
+                user.save()
+                .then(result => {
+                    // Handle the result
+                    console.log("done registration",result);
+                    let data="registration done";
+                    res.render('views/home',{})
+                  })
+                  .catch(err => {
+                    // Handle the error
+                    console.error("Faild to register db",err);
+                  });
+
+
+                
+
+            }else {
+                // Handle non-200 status code
+                console.error("Weather API returned non-200 status code:", response.statusCode);
+                // Send an error response to the client if needed
+                res.status(500).send('Internal Server Error');
+            }
+        }) 
+
+
+     });
+   }catch(error){
+    console.log("error in fetching bank data",error)
+   }
+
+ }
+
+
+
+
+ module.exports = router;
---
+++ dbmodel/db.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
+
+// Create a Mongoose connection
+try{
+  
+  const connection = mongoose.createConnection('mongodb+srv://ciriktazumo:iwYUCXCu3sm1bQuE@joshua.gs3va5s.mongodb.net/');
+
+// Initialize mongoose-auto-increment with the Mongoose connection
+autoIncrement.initialize(connection);
+
+// Create the user schema
+const userSchema = new mongoose.Schema({
+  user_id: { type: Number, unique: true },
+  user_name: String,
+  bank_accounts: [String],
+  name: String,
+  accounts: {
+    bank: String,
+    branch: String,
+    address: String,
+    city: String,
+    district: String,
+    state: String,
+    bank_code: String,
+    weather: {
+      temp: Number,
+      humidity: Number
+    }
+  }
+});
+
+
+
+
+
+// Create the User model
+const UserModel = connection.model('User', userSchema);
+console.log("connected");
+
+// Export the User model
+module.exports = UserModel;
+
+}catch(error){
+  console.log("error in db.js",error.message);
+}

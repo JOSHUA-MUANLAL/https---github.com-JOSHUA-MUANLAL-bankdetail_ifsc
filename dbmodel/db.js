b86fc4df15a944d5d8dbb465f83b9ea83e49b871
JOSHUA-MUANLAL <105428544+JOSHUA-MUANLAL@users.noreply.github.com>
2024-01-26 00:44:01 +0530

    Update db.js

--- dbmodel/db.js
+++ dbmodel/db.js
@@ -4,7 +4,7 @@ const autoIncrement = require('mongoose-auto-increment');
 // Create a Mongoose connection
 try{
   
- const connection = mongoose.createConnection('mongodb+srv://ciriktazumo:iwYUCXCu3sm1bQuE@joshua.gs3va5s.mongodb.net/');
+ const connection = mongoose.createConnection('mongodb+srv://ciriktazumo:iwYUCXCu3sm1bQuE@joshua.gs3va5s.mongodb.net/test');
 
 // Initialize mongoose-auto-increment with the Mongoose connection
 autoIncrement.initialize(connection);
